2c3d49c3586859e70a2fc5b7b8ead861809beeff
Paige Ruppel <paige.ruppel@gmail.com>
2018-06-13 12:47:06 -0400

    passing second test

--- src/main/java/com/paigeruppel/katas/bookdiscount/DiscountCalculator.java
+++ src/main/java/com/paigeruppel/katas/bookdiscount/DiscountCalculator.java
@@ -1,9 +1,35 @@
 package com.paigeruppel.katas.bookdiscount;
 
 import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class DiscountCalculator {
+
+    private static final BigDecimal RAW_COST_PER_BOOK = BigDecimal.valueOf(8.00);
+
     public BigDecimal getCost(int[] booksToPurchase) {
-        return BigDecimal.valueOf(8.00);
+
+        int numberOfBooks = Arrays.stream(booksToPurchase).reduce(0, (x,y) -> x + y);
+        BigDecimal discount = findDiscount(numberOfBooks);
+        BigDecimal rawTotalPrice = RAW_COST_PER_BOOK.multiply(BigDecimal.valueOf(numberOfBooks));
+        BigDecimal discountedPrice = rawTotalPrice.multiply(discount);
+        return discountedPrice.setScale(2);
+    }
+
+    private BigDecimal findDiscount(int numberOfBooks) {
+        return discountMap().get(numberOfBooks);
+    }
+
+    private static Map<Integer, BigDecimal> discountMap() {
+        Map<Integer, BigDecimal> discounts = new HashMap<>();
+        discounts.put(1, BigDecimal.valueOf(1.00));
+        discounts.put(2, BigDecimal.valueOf(0.95));
+        discounts.put(3, BigDecimal.valueOf(0.90));
+        discounts.put(4, BigDecimal.valueOf(0.80));
+        discounts.put(5, BigDecimal.valueOf(0.75));
+        return discounts;
     }
 }
--- src/test/java/com/paigeruppel/katas/bookdiscount/DiscountCalculatorTest.java
+++ src/test/java/com/paigeruppel/katas/bookdiscount/DiscountCalculatorTest.java
@@ -9,11 +9,21 @@ import static org.junit.Assert.assertThat;
 
 public class DiscountCalculatorTest {
 
+    BigDecimal eightDollars = BigDecimal.valueOf(8.00).setScale(2);
+    BigDecimal fifteenDollarsTwentyCents = BigDecimal.valueOf(15.20).setScale(2);
+
     @Test
     public void shouldReturn8WhenOnlyOneBookIsPurchased() {
         DiscountCalculator underTest = new DiscountCalculator();
         int[] booksToPurchase = {1, 0, 0, 0, 0};
-        assertThat(underTest.getCost(booksToPurchase), is(BigDecimal.valueOf(8.00)));
+        assertThat(underTest.getCost(booksToPurchase), is(eightDollars));
+    }
+
+    @Test
+    public void shouldApplyA5PercentDiscountToTwoDifferentBooks() {
+        DiscountCalculator underTest = new DiscountCalculator();
+        int[] booksToPurchase = {1, 1, 0, 0, 0};
+        assertThat(underTest.getCost(booksToPurchase), is(fifteenDollarsTwentyCents));
     }
 
 }
