3d1ae2cc1e3e7dd90caf017f7bff1c0cff385cf7
Paige Ruppel <paige.ruppel@gmail.com>
2018-06-13 12:51:24 -0400

    tests passing for each possible discount - before writing logic to detect duplicate books

--- src/test/java/com/paigeruppel/katas/bookdiscount/DiscountCalculatorTest.java
+++ src/test/java/com/paigeruppel/katas/bookdiscount/DiscountCalculatorTest.java
@@ -11,6 +11,9 @@ public class DiscountCalculatorTest {
 
     BigDecimal eightDollars = BigDecimal.valueOf(8.00).setScale(2);
     BigDecimal fifteenDollarsTwentyCents = BigDecimal.valueOf(15.20).setScale(2);
+    BigDecimal twentyOneDollarsSixtyCents = BigDecimal.valueOf(21.60).setScale(2);
+    BigDecimal twentyFiveDollarsSixtyCents = BigDecimal.valueOf(25.60).setScale(2);
+    BigDecimal thirtyDollars = BigDecimal.valueOf(30.00).setScale(2);
 
     @Test
     public void shouldReturn8WhenOnlyOneBookIsPurchased() {
@@ -24,6 +27,27 @@ public class DiscountCalculatorTest {
         DiscountCalculator underTest = new DiscountCalculator();
         int[] booksToPurchase = {1, 1, 0, 0, 0};
         assertThat(underTest.getCost(booksToPurchase), is(fifteenDollarsTwentyCents));
+    }
+
+    @Test
+    public void shouldApplyA10PercentDiscountToThreeDifferentBooks() {
+        DiscountCalculator underTest = new DiscountCalculator();
+        int[] booksToPurchase = {1, 1, 1, 0, 0};
+        assertThat(underTest.getCost(booksToPurchase), is(twentyOneDollarsSixtyCents));
+    }
+
+    @Test
+    public void shouldApplyA20PercentDiscountToFourDifferentBooks() {
+        DiscountCalculator underTest = new DiscountCalculator();
+        int[] booksToPurchase = {1, 1, 1, 1, 0};
+        assertThat(underTest.getCost(booksToPurchase), is(twentyFiveDollarsSixtyCents));
+    }
+
+    @Test
+    public void shouldApplyA25PercentDiscountToFiveDifferentBooks() {
+        DiscountCalculator underTest = new DiscountCalculator();
+        int[] booksToPurchase = {1, 1, 1, 1, 1};
+        assertThat(underTest.getCost(booksToPurchase), is(thirtyDollars));
     }
 
 }
